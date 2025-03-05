2a78fdb25e5f2a3ccfbe69314a26c9d2d29499b2
frank <liuguozhu720@gmail.com>
2025-03-05 18:27:30 +0800

    change tft config

--- User_Setup.h
+++ User_Setup.h
@@ -5,9 +5,10 @@
 #define TFT_CS 15
 #define TFT_DC 2
 #define TFT_RST 4
-#define TFT_MOSI 11
-#define TFT_SCLK 12
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_MISO -1 // 禁用 MISO
 // 不定义 TFT_BL，因为 LED 已接 5V
 
-#define SPI_FREQUENCY 8000000
+#define SPI_FREQUENCY 4000000
 #define TFT_DEBUG
